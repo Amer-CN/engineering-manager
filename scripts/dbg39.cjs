ce8cf23b74b1b339aba6095695642092683fbe92
Amer <Amer@local>
2026-06-19 18:32:39 +0800

    v0.74.0 WIP (restored after mimo wrapper accident; pre-v0.74.0 history lost)

--- scripts/dbg39.cjs
+++
@@ -1,4 +0,0 @@
-const c = require("fs").readFileSync("EngineeringManager.Api/Endpoints/PartnerEndpoints.cs", "utf8")
-const start3 = c.indexOf("        app.MapDelete(\"/api/partners/{id}\"")
-const semi3 = c.indexOf(";", start3)
-console.log("start3:", start3, "first ;:", semi3, "dist:", semi3 - start3)
