f1e6b72f4fa1418b5b2cddbd63b98f4622f29344
Amer <Amer@local>
2026-07-30 00:35:34 +0800

    chore: bump version to 0.85.3

--- installer/src/App.tsx
+++ installer/src/App.tsx
@@ -119,7 +119,7 @@ export default function App() {
           style={{ position: 'absolute', inset: 0 }}
         >
           {step === 'welcome' && (
-            <WelcomeStep onBegin={handleBegin} version="0.85.2" />
+            <WelcomeStep onBegin={handleBegin} version="0.85.3" />
           )}
           {step === 'path' && (
             <PathStep
